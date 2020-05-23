fd74b3297894303185c976d1b0cacbe047e1bc80
flyCode—-Lucky <1424676006@qq.com>
2020-05-23 16:47:16 +0800

    问题发布功能

--- src/main/java/com/mystudy/community/community/controller/PublishController.java
+++ src/main/java/com/mystudy/community/community/controller/PublishController.java
@@ -1,7 +1,19 @@
 package com.mystudy.community.community.controller;
 
+import com.mysql.cj.Session;
+import com.mystudy.community.community.mapper.QuesstionMapper;
+import com.mystudy.community.community.mapper.UserMapper;
+import com.mystudy.community.community.model.Question;
+import com.mystudy.community.community.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @描述
@@ -11,9 +23,58 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PublishController {
 
+    @Resource
+    private QuesstionMapper quesstionMapper;
+    @Resource
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish(){
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model){
+
+        //往question里填充数据
+        Question question = new Question();
+        //这三个是publish.html页面里写的参数，传过来直接用
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        //该条记录创建与修改时间
+        question.setGmt_create(System.currentTimeMillis());
+        question.setGmt_modified(question.getGmt_create());
+
+        //获取user
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token=cookie.getValue();
+                user = userMapper.findByToken(token);
+                if(user!=null){
+                    request.getSession().setAttribute("user",user);
+                    /* 发布者id */
+                    question.setCreator(user.getId());
+                }
+                break;
+            }
+        }
+        if(user==null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+
+        //将准备好的question插入数据库
+        quesstionMapper.creat(question);
+        return "redirect:/";
     }
 
 }
---
+++ src/main/java/com/mystudy/community/community/mapper/QuesstionMapper.java
@@ -0,0 +1,17 @@
+package com.mystudy.community.community.mapper;
+
+import com.mystudy.community.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @描述
+ * @创建人 CYH
+ * @创建时间 2020/5/23
+ */
+@Mapper
+public interface QuesstionMapper {
+
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag) values(#{title},#{description},#{gmt_create},#{gmt_modified},#{creator},#{tag})")
+    void creat(Question question);
+}
